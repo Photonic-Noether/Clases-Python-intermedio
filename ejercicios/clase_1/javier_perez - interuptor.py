918c5df0c3708f8e58f0c573f6759f6776ca0188
Javier <javierperez@perasalvino.es>
2025-09-23 19:55:43 +0200

    new file:   ejercicios/clase_1/javier_perez - interuptor.py

---
+++ ejercicios/clase_1/javier_perez - interuptor.py
@@ -0,0 +1,14 @@
+
+
+
+variable_de_paso = input('¿Nuevo estado del interructor (True/False)? ')
+
+
+
+#* con if
+if variable_de_paso.lower()  == 'true':
+    print('Entra con if')
+else:
+    print('No entra con if')
+    
+    
