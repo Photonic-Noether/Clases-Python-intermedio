94324cc61d27d1653549b21a9ea8edda540ad7c5
Veronica <vpalmah@gmail.com>
2025-09-23 19:47:32 +0200

    interruptor Verónica

---
+++ Interruptor.py
@@ -0,0 +1,11 @@
+def __init__ (self):
+    self.status = False
+
+def encender (self):
+    self.status = True
+
+def apagar (self):
+    self.status = False
+
+def esta_encendido (self):
+    return self.status
