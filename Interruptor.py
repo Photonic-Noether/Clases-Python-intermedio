4f76fefbf759722b1a29ce25993e88b3606d23f2
Verónica P <134886948+AliceKyte@users.noreply.github.com>
2025-09-23 20:09:47 +0200

    Inclosa la lògica i funció d'estat

--- Interruptor.py
+++ Interruptor.py
@@ -1,11 +1,27 @@
-def __init__ (self):
-    self.status = False
+class Interruptor:
 
-def encender (self):
-    self.status = True
+    def __init__ (self):
+        self.status = False
 
-def apagar (self):
-    self.status = False
+    def encender(self):
+        if not self.estado: 
+            self.estado = True
+            print("Encendido")
+        else:
+            print("Ya estaba encendido")
+    
+    def apagar(self):
+        if self.estado: 
+            self.estado = False
+            print("Está apagado")
+        else:
+            print("Ya estaba apagado")
 
-def esta_encendido (self):
-    return self.status
+    def estado (self):
+        return self.status
+    
+    def info (self):
+        if self.status:
+            print("El interruptor está encendido")
+        else:
+            print("El interruptor está apagado")
