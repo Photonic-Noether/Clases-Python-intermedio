1797acd2995c3e9157db2bb2c379debcd3fd7fbb
martnz.fer@gmail.com <martnz.fer@gmail.com>
2025-09-24 18:01:52 +0200

    version mejorada con interruptor ya funcionando

--- ejercicios/clase_1/interruptor.py
+++ ejercicios/clase_1/interruptor.py
@@ -1,1 +1,28 @@
-print("prueba interruptor tipo hola mundo: Fernando martinez")
+class Interruptor():
+    def __init__(self,apagado=True):
+        self.apagado = False if apagado == True else False
+    def estado(self):
+        if self.apagado:
+            print("ON")
+        else: 
+            print("OFF")
+    
+    def conmutar(self):
+        NuevoeEstado = not self.apagado
+        print("nuevo Estado: ", NuevoeEstado)
+        self.apagado = True if NuevoeEstado else False
+
+
+
+
+print("prueba interruptor Fernando martinez")
+interr = Interruptor(True) #inicio variable con Interruptor apagado
+interr.estado()
+print("Enciendo")
+interr.conmutar()
+interr.estado()
+print("vuelvo a darle (Apago)")
+interr.conmutar()
+interr.estado()
+
+
